fecdcce6c3d546366b7b585e4ee30ee2633b98ec
Winston Feng <tifish@gmail.com>
2023-04-04 16:55:01 +0800

    New CustomizedTools can add customized features in the Scene Design Tools window.

---
+++ Assets/Scripts/Editor/SceneDesignToolsCustomizeTools.cs
@@ -0,0 +1,30 @@
+﻿using SceneDesignTools;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class SceneDesignToolsCustomizeTools
+{
+    static SceneDesignToolsCustomizeTools()
+    {
+        CustomizedTools.AddCustomTools(OnGUI);
+    }
+
+    private static bool _expanded;
+
+    private static void OnGUI()
+    {
+        _expanded = EditorGUILayout.Foldout(_expanded, "External Bot", EditorGUIHelper.FoldoutStyle);
+        if (_expanded)
+        {
+            EditorGUIHelper.BeginIndent();
+
+            if (GUILayout.Button("Hello"))
+                EditorUtility.DisplayDialog("Hello", "Hello World!", "OK");
+
+            EditorGUIHelper.EndIndent();
+        }
+
+        EditorGUIHelper.SeparatorLine();
+    }
+}
---
+++ Packages/com.tifish.scenedesigntools/Editor/CustomizedTools.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SceneDesignTools
+{
+    public class CustomizedTools : BaseSceneDesignTool
+    {
+        public CustomizedTools(SceneDesignToolsWindow ownerWindow) : base(ownerWindow)
+        {
+        }
+
+        private static readonly List<Action> CustomizedToolsCallbacks = new List<Action>();
+
+        public static void AddCustomTools(Action callback)
+        {
+            if (CustomizedToolsCallbacks.Contains(callback))
+                return;
+
+            CustomizedToolsCallbacks.Add(callback);
+        }
+
+        public override void OnGUI()
+        {
+            foreach (var customizedToolsCallback in CustomizedToolsCallbacks)
+                customizedToolsCallback();
+        }
+    }
+}
